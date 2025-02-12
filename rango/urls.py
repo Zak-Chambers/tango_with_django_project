46137c4b35330a595437d735ac8e5523d1ff02ef
Zak Boulaich <2879545b@student.gla.ac.uk>
2025-02-12 01:23:31 +0000

    chapter 4 done

--- rango/urls.py
+++ rango/urls.py
@@ -6,4 +6,4 @@ app_name = 'rango'
 urlpatterns = [
     path('', views.index, name= 'index'),
     path('about/', views.about, name='about'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
