809ea6b48581f907faf81aa0416e16739fa93574
Zak Boulaich <2879545b@student.gla.ac.uk>
2025-02-14 02:12:52 +0000

    Chapter 7 done

--- rango/urls.py
+++ rango/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 from rango import views
-from django.urls import include
-from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
 
-app_name = 'rango'
+app_name = 'rango'  
 
 urlpatterns = [
-    path('', views.index, name= 'index'),
+    path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('add_category/', views.add_category, name='add_category'), 
+    path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),  
+]
