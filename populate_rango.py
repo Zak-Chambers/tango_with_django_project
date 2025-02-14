f7282760f3442df87e61e5eee804b93751a012c9
Zak Boulaich <2879545b@student.gla.ac.uk>
2025-02-14 00:24:11 +0000

    Chapter 6 done

--- populate_rango.py
+++ populate_rango.py
@@ -5,39 +5,9 @@ import django
 django.setup()
 
 from rango.models import Category, Page
+from django.utils.text import slugify
 
-def populate():
-    python_pages = [
-        {'title': 'Official Python Tutorial', 'url': 'http://docs.python.org/3/tutorial/'},
-        {'title': 'How to Think like a Computer Scientist', 'url': 'http://www.greenteapress.com/thinkpython/'},
-        {'title': 'Learn Python in 10 Minutes', 'url': 'http://www.korokithakis.net/tutorials/python/'}
-    ]
 
-    django_pages = [
-        {'title': 'Official Django Tutorial', 'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-        {'title': 'Django Rocks', 'url': 'http://www.djangorocks.com/'},
-        {'title': 'How to Tango with Django', 'url': 'http://www.tangowithdjango.com/'}
-    ]
-
-    other_pages = [
-        {'title': 'Bottle', 'url': 'http://bottlepy.org/docs/dev/'},
-        {'title': 'Flask', 'url': 'http://flask.pocoo.org'}
-    ]
-
-    cats = {
-        'Python': {'pages': python_pages,'views':128, 'likes': 64},
-        'Django': {'pages': django_pages, 'views':64, 'likes': 32},
-        'Other Frameworks': {'pages': other_pages,'views':32, 'likes': 16}
-    }
-
-    for cat, cat_data in cats.items():
-        c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
-        for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
-
-    for c in Category.objects.all():
-        for p in Page.objects.filter(category=c):
-            print(f'- {c}: {p}')
 
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
@@ -50,8 +20,39 @@ def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
     c.views = views
     c.likes = likes
+    c.slug = slugify(name)
     c.save()
     return c
+
+def populate():
+    python_cat = add_cat('Python', views=128, likes=64)
+
+    add_page(python_cat, "Official Python Tutorial",
+             "http://docs.python.org/3/tutorial/", views=50)
+    add_page(python_cat, "How to Think like a Computer Scientist",
+             "http://www.greenteapress.com/thinkpython/", views=45) 
+    add_page(python_cat, "Learn Python",
+             "http://www.learnpython.org/", views=40)
+
+    django_cat = add_cat('Django', views=64, likes=32)
+
+    add_page(django_cat, "Official Django Tutorial",
+             "https://docs.djangoproject.com/en/2.1/intro/tutorial01/", views=80)
+    add_page(django_cat, "Django Rocks",
+             "http://www.djangorocks.com/", views=60)
+    add_page(django_cat, "How to Tango with Django",
+             "http://www.tangowithdjango.com/", views=70)  
+
+    other_cat = add_cat('Other Frameworks', views=32, likes=16)
+
+    add_page(other_cat, "Bottle",
+             "http://bottlepy.org/docs/dev/", views=25)  
+    add_page(other_cat, "Flask",
+             "http://flask.pocoo.org", views=20)
+    
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print(f'- {c}: {p}')
 
 if __name__ == '__main__':
     print('Starting Rango population script...')
--- rango/urls.py
+++ rango/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from rango import views
+from django.urls import include
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name= 'index'),
     path('about/', views.about, name='about'),
-]
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
