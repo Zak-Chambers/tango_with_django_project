0a523b4be3a61ecc6d4a6c327d1f91cb0f55dcec
Zak Boulaich <2879545b@student.gla.ac.uk>
2025-02-14 12:51:49 +0000

    Full project done

--- rango/admin.py
+++ rango/admin.py
@@ -8,8 +8,6 @@ class PageAdmin (admin.ModelAdmin):
 
 admin.site.register(Page, PageAdmin)
 
-# Add in this class to customise the Admin Interface
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
-# Update the registration to include this customised interface
 admin.site.register(Category, CategoryAdmin)
--- rango/views.py
+++ rango/views.py
@@ -31,53 +31,29 @@ def about(request):
     return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
-    # Create a context dictionary which we can pass
-    # to the template rendering engine.
     context_dict = {}
 
     try:
-        # Can we find a category name slug with the given name?
-        # If we can't, the .get() method raises a DoesNotExist exception.
-        # The .get() method returns one model instance or raises an exception.
         category = Category.objects.get(slug=category_name_slug)
-        # Retrieve all of the associated pages.
-        # The filter() will return a list of page objects or an empty list.
         pages = Page.objects.filter(category=category)
-        # Adds our results list to the template context under name pages.
         context_dict['pages'] = pages
-        # We also add the category object from
-        # the database to the context dictionary.
-        # We'll use this in the template to verify that the category exists.
         context_dict['category'] = category
     except Category.DoesNotExist:
-        # We get here if we didn't find the specified category.
-        # Don't do anything -
-        # the template will display the "no category" message for us.
         context_dict['category'] = None
         context_dict['pages'] = None
-        # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context=context_dict)
 
 @login_required
 def add_category(request):
     form = CategoryForm()
-    # A HTTP POST?
     if request.method == 'POST':
         form = CategoryForm(request.POST)
-        # Have we been provided with a valid form?
         if form.is_valid():
-            # Save the new category to the database.
             form.save(commit=True)
-            # Now that the category is saved, we could confirm this.
-            # For now, just redirect the user back to the index view.
             return redirect('/rango/')
         else:
-            # The supplied form contained errors -
-            # just print them to the terminal.
             print(form.errors)
 
-    # Will handle the bad form, new form, or no form supplied cases.
-    # Render the form with error messages (if any).
     return render(request, 'rango/add_category.html', {'form': form})
 
 @login_required
@@ -87,7 +63,6 @@ def add_page(request, category_name_slug):
     except Category.DoesNotExist:
         category = None
 
-    # You cannot add a page to a Category that does not exist...
     if category is None:
         return redirect('/rango/')
     
@@ -140,42 +115,20 @@ def register(request):
                       'profile_form': profile_form,
                       'registered': registered})
 def user_login(request):
-    # If the request is a HTTP POST, try to pull out the relevant information.
     if request.method =='POST':
-        # Gather the username and password provided by the user.
-        # This information is obtained from the login form.
-        # We use request.POST.get('<variable>') as opposed
-        # to request.POST['<variable>'], because the
-        # request.POST.get('<variable>') returns None if the
-        # value does not exist, while request.POST['<variable>']
-        # will raise a KeyError exception.
         username = request.POST.get('username')
         password = request.POST.get('password')
-        # Use Django's machinery to attempt to see if the username/password
-        # combination is valid - a User object is returned if it is.
         user = authenticate(username=username, password=password)
-        # If we have a User object, the details are correct.
-        # If None (Python's way of representing the absence of a value), no user
-        # with matching credentials was found.
         if user:
-            # Is the account active? It could have been disabled.
             if user.is_active:
-                # If the account is valid and active, we can log the user in.
-                # We'll send the user back to the homepage.
                 login(request, user)
                 return redirect(reverse('rango:index'))
             else:
-                # An inactive account was used - no logging in!
                 return HttpResponse("Your Rango account is disabled.")
         else:
-            # Bad login details were provided. So we can't log the user in.
             print(f"Invalid login details: {username}, {password}")
             return HttpResponse("Invalid login details supplied.")
-    # The request is not a HTTP POST, so display the login form.
-    # This scenario would most likely be a HTTP GET.
     else:
-        # No context variables to pass to the template system, hence the
-        # blank dictionary object...
         return render(request, 'rango/login.html')
 
 def some_view(request):
@@ -188,23 +141,16 @@ def some_view(request):
 def restricted(request):
     return render(request, 'rango/restricted.html')
 
-# Use the login_required() decorator to ensure only those logged in can
-# access the view.
 @login_required
 def user_logout(request):
-    # Since we know the user is logged in, we can now just log them out.
     logout(request)
-    # Take the user back to the homepage.
     return redirect(reverse('rango:index'))
 
-# A helper method
 def get_server_side_cookie(request, cookie, default_val=None): 
     val = request.session.get(cookie)
     if not val:
         val = default_val
     return val
-
-# Updated the function definition
 def visitor_cookie_handler(request):
     visits = int(get_server_side_cookie(request, 'visits','1'))
     last_visit_cookie = get_server_side_cookie(request,
@@ -213,13 +159,9 @@ def visitor_cookie_handler(request):
     last_visit_time = datetime.strptime(last_visit_cookie[:-7],
                                     '%Y-%m-%d %H:%M:%S')
 
-    # If it's been more than a day since the last visit...
     if (datetime.now() - last_visit_time).days > 0:
         visits = visits + 1
-        # Update the last visit cookie now that we have updated the count
         request.session['last_visit'] = str(datetime.now())
     else:
-        # Set the last visit cookie
         request.session['last_visit'] = last_visit_cookie
-    # Update/set the visits cookie
     request.session['visits'] = visits
